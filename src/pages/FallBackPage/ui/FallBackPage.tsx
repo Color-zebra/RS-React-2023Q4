c1088d46f316b08cb8e33f5ce1732296e54d3a43
Romanenkov Dmitriy <215rd@mail.ru>
2023-10-28 22:30:22 +0300

    feat: add fallback page styles

--- src/pages/FallBackPage/ui/FallBackPage.tsx
+++ src/pages/FallBackPage/ui/FallBackPage.tsx
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
+import { RootButton } from '../../../shared/ui/RootButton';
+import classes from './FallBackPage.module.scss';
+import img from '../../../shared/assets/images/fallback-back.jpg';
 
 export default class FallBackPage extends Component {
+  updatePage() {
+    location.reload();
+  }
   render() {
-    return <div>I&apos;m fallback interface</div>;
+    return (
+      <div className={classes.page} style={{ backgroundImage: `url(${img})` }}>
+        <div className={classes.content}>
+          <p>Looks like everything is broken :&#40;</p>
+          <RootButton onClick={this.updatePage}>Update page</RootButton>
+        </div>
+      </div>
+    );
   }
 }
