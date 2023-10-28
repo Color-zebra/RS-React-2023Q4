c8baddde078ab663ed756a72fb0d159b6b3c65df
Romanenkov Dmitriy <215rd@mail.ru>
2023-10-28 22:46:14 +0300

    feat: add NothingFound component

--- src/widgets/Content/ui/Content.tsx
+++ src/widgets/Content/ui/Content.tsx
@@ -1,8 +1,9 @@
-import { Component } from 'react';
+import { Component, ReactNode } from 'react';
 import { CharacterCard } from '../../../entities/CharacterCard';
 import Spinner from '../../../shared/ui/Spinner/Spinner';
 import { Character } from '../../../shared/types/types';
 import classes from './Content.module.scss';
+import NothingFound from '../../../entities/NothingFound/NothingFound/NothingFound';
 
 type Props = {
   isReady: boolean;
@@ -16,14 +17,22 @@ export default class Content extends Component<Props> {
 
   render() {
     const { isReady, characters } = this.props;
+    let res: ReactNode;
+    if (characters && characters.length !== 0) {
+      res = (
+        <div className={classes.layout}>
+          {characters.map((char) => (
+            <CharacterCard key={char.id} {...char} />
+          ))}
+        </div>
+      );
+    } else {
+      res = <NothingFound />;
+    }
     return (
       <>
         {isReady ? (
-          <div className={classes.layout}>
-            {characters.map((char) => (
-              <CharacterCard key={char.id} {...char} />
-            ))}
-          </div>
+          res
         ) : (
           <div className={classes.spinner}>
             <Spinner />
